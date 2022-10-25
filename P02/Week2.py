98886b299b81fff1a15726ef14bfed36935d5afd
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-10-25 18:20:09 +0800

    Update and rename evenodd.py to Week2.py

---
+++ P02/Week2.py
@@ -0,0 +1,71 @@
+# for number in range(8, 2, -1):
+#     print(number, end=" ")
+
+#Question 1
+# number = input("Enter a number: ")
+
+# if (number.isnumeric() == False):
+#     print("You did not enter a number")
+# elif (int(number) % 2 != 0):
+#     print(f"{number} is odd")
+# else:
+#     print(f"{number} is even")
+
+#Question 2
+# number1 = input("Enter a number: ")
+# number2 = input("Enter another number: ")
+
+# if (number1.isnumeric() == False):
+#     print("You didn't enter a number")
+# elif (number2.isnumeric() == False):
+#     print("You didn't enter a number")
+# elif (int(number1) > int(number2)):
+#     print(f"{number1} is bigger!")
+# elif (int(number1) < int(number2)):
+#     print(f"{number2} is bigger!")
+# else:
+#     print("Both numbers are the same!")
+
+# #Question 3
+# years = input("Enter number of years in service: ")
+# salary = input("Enter current salary: ")
+
+# if (years.isnumeric() == False):
+#     print("It is not a number")
+# elif (salary.isnumeric() == False):
+#     print("It is not a number")
+# else:
+#     if (int(years) < 10):
+#         if (int(salary) < 1000):
+#             print("Your increment is $100")
+#         elif (int(salary) < 2000):
+#             print("Your increment is $200")
+#         else:
+#             print("Your increment is $300")
+#     if (int(years) > 10):
+#         if (int(salary) < 1000):
+#             print("Your increment is $200")
+#         elif (int(salary) < 2000):
+#             print("Your increment is $300")
+#         else:
+#             print("Your increment is $400")
+#Question 4
+number1 = int(input("Enter a starting number: "))
+number2 = int(input("Enter a ending number: "))
+
+if (number1 % 2 == 0):
+    number1 + 1
+    if (number1 < number2):
+        for i in range(number1, number2, 2):
+            print(i,end = "\t")
+    else:
+        for j in range(number2, number1, -2):
+            print(j,end = "\t")
+if (number2 % 2 == 0):
+    number2 + 1
+    if (number1 < number2):
+        for i in range(number1, number2, 2):
+            print(i,end = "\t")
+    else:
+        for j in range(number2, number1, -2):
+            print(j,end = "\t")
