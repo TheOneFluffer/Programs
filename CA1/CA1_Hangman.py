9955eb784472e1e370ce52fb20a2680bd1854928
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-11-04 17:33:00 +0800

    Add files via upload

---
+++ CA1/CA1_Hangman.py
@@ -0,0 +1,4 @@
+difficulty_settings = input("Please enter a difficulty:\n 1. Easy\n 2. Medium\n 3. Hard\n > ")
+selection = input("")
+username = input("Player: ")
+userKeyed = input("Select a valid character [a-z,']: ")
