d8059a6a02f2617dfba7010d773c450e2c5218a1
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-11-14 23:08:59 +0800

    Delete CA1 directory

--- CA1/CA1_Hangman.py
+++
@@ -1,4 +0,0 @@
-difficulty_settings = input("Please enter a difficulty:\n 1. Easy\n 2. Medium\n 3. Hard\n > ")
-selection = input("")
-username = input("Player: ")
-userKeyed = input("Select a valid character [a-z,']: ")
--- CA1/Game_settings.py
+++
@@ -1,4 +0,0 @@
-game_settings:
-    "number of attempts": 3,
-    "number of words": 3,
-    "number of top players": 4
