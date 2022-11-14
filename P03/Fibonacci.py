ff2d690c8a06ef0085c8af1abf0860491e88822e
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-11-14 23:07:27 +0800

    I like doogal

---
+++ P03/Fibonacci.py
@@ -0,0 +1,9 @@
+def fibs(n):
+    #  Insert your codes here
+    fib_list = []
+    index1 = 0
+    index2 = 1
+    while len(fib_list) < n:
+        if 
+
+print(fibs(5))	# output = [0, 1, 1, 2, 3]; argument=5 > sequence of 5 values
--- P03/Random.py
+++ P03/Random.py
@@ -1,9 +1,22 @@
 import random
 
-def random_list(end, start):
+def random_list(total_val, start):
     rand_list = []
+    while len(rand_list) < total_val:
+        if start == 0 or start == None:
+            rand_num = random.randint(start, total_val-1)
+        else:
+            rand_num = random.randint(start, total_val + (start-1))
+        if rand_num not in rand_list:
+            rand_list.append(rand_num)
+    return rand_list
     
 
 for i in range(3):
-    list = random_list(10, 0) # the second argument is optional, default is 0
+    list = random_list(8, 5) # the second argument is optional, default is 0
     print(list)
+
+
+# for i in range(10):
+    
+#     print(random.randrange(0, 10), end=" ")
